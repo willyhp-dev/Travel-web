362d51418cf8e566f58043f6494799c63a690051
willyhp123 <limwilly0@gmail.com>
2022-06-30 13:42:23 +0700

    update

--- src/mainPage/index.jsx
+++ src/mainPage/index.jsx
@@ -1,38 +1,58 @@
-import {Sliders,CardFeatures,Content,Footer} from "./CustomHooks.js";
-import {Container,Row,Col,Button} from "react-bootstrap";
-import {Navbars} from "./CustomHooks.js";
+import { Sliders, CardFeatures, Content, Footer } from "./CustomHooks.js";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { Navbars } from "./CustomHooks.js";
+import { Link } from "react-router-dom";
 
-import "./index.css"
+import "./index.css";
 export default function TodoList() {
-
- 
   return (
     <>
-     <Navbars/>
-     <Container>
-     <Sliders/>
-     <h3 className ="mt-3"><center>FEATURES</center></h3>
-     <div className="borders"></div>
-     <Row>
-      <Col sm={4}><CardFeatures/></Col>
-      <Col sm={4}><CardFeatures/></Col>
-      <Col sm={4}><CardFeatures/></Col>
-     </Row>
-     <h3 className ="mt-3"><center>Content</center></h3>
-     <div className="borders"></div>
-     <Row>
-      <Col sm={4}><Content/></Col>
-      <Col sm={4}><Content/></Col>
-      <Col sm={4}><Content/></Col>
-     </Row>
-     <Row className="mb-5">
-      <Col sm={4}></Col>
-      <Col sm={4}><center><Button className="mt-5">More</Button></center></Col>
-      <Col sm={4}></Col>
-     </Row>
-     </Container>
-     <Footer/>
-    
+      <Navbars />
+      <Container>
+        <Sliders />
+        <h3 className="mt-3">
+          <center>FEATURES</center>
+        </h3>
+        <div className="borders"></div>
+        <Row>
+          <Col sm={4}>
+            <CardFeatures />
+          </Col>
+          <Col sm={4}>
+            <CardFeatures />
+          </Col>
+          <Col sm={4}>
+            <CardFeatures />
+          </Col>
+        </Row>
+        <h3 className="mt-3">
+          <center>Content</center>
+        </h3>
+        <div className="borders"></div>
+        <Row>
+          <Col sm={4}>
+            <Content />
+          </Col>
+          <Col sm={4}>
+            <Content />
+          </Col>
+          <Col sm={4}>
+            <Content />
+          </Col>
+        </Row>
+        <Row className="mb-5">
+          <Col sm={4}></Col>
+          <Col sm={4}>
+            <center>
+              <Link to="/content" element>
+                <Button className="mt-5">More</Button>
+              </Link>
+            </center>
+          </Col>
+          <Col sm={4}></Col>
+        </Row>
+      </Container>
+      <Footer />
     </>
   );
 }
